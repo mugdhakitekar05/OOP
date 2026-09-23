04b33638c03271acd5927466f03adb37b37e9ddf
mugdhakitekar05 <mugdhakitekar@gmail.com>
2026-09-24 02:27:09 +0530

    Add files via upload

---
+++ Tut 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+
+    for (int i = 1; i <= 50; i++) {
+        if (i % 2 == 0) {
+            sum = sum + i;
+        }
+    }
+
+    cout << "Sum of even numbers between 1 and 50 = " << sum;
+
+    return 0;
+}
